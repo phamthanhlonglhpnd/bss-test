d14eafd1e6f8ccd1df469b4aceac44c7b151b8f5
phamthanhlonglhpnd <86229902+phamthanhlonglhpnd@users.noreply.github.com>
2024-08-18 20:22:50 +0700

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import FlashSale from './components/FlashSale'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <FlashSale />
     </>
   )
 }
